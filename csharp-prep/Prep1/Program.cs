837de83be88c58b4740687310d661ad95e04e493
Jamian221 <ols23045@byui.edu>
2024-11-04 09:20:06 -0700

    I finished part one of the week one prove assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // declare variables
+        string firstName;
+        string lastName;
+        // get first name
+        Console.Write("What is your first name? ");
+        firstName = Console.ReadLine();
+        // get last name
+        Console.Write("What is your last name? ");
+        lastName = Console.ReadLine();
+        // line space
+        Console.WriteLine("");
+        
+
+        Console.Write($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
