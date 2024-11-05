143a3a123ce502ac627591fec9507d65763c0f71
Jamian221 <ols23045@byui.edu>
2024-11-05 10:01:32 -0700

    Finished prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,44 @@ using System;
 
 class Program
 {
+    static int GuessNumber(){
+        Console.Write("Guess a number: ");
+        int guessedNumber = int.Parse(Console.ReadLine());
+        return guessedNumber;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // declare var
+        int magicNumber;
+        int guessedNumber;
+        bool keepPlaying;
+        string playAgain;
+        // get magic number
+        do{
+            Console.Write("What is your magic number? ");
+            magicNumber = int.Parse(Console.ReadLine());
+
+            do
+            {
+                // retrieve a number
+                guessedNumber = GuessNumber();
+
+                // test and tell the user whether they guess it, higher or lower
+                if (guessedNumber == magicNumber){
+                    Console.WriteLine("You guessed it!");
+                } else if (guessedNumber > magicNumber){
+                    Console.WriteLine("Lower");
+                } else if (guessedNumber < magicNumber){
+                    Console.WriteLine("Higher");
+                }
+            }while(guessedNumber != magicNumber); // restart the loop if they did not guess it
+
+                // ask if they would like to play again and repeat if they say "yes"
+                Console.Write("Want to play again? ");
+                playAgain = Console.ReadLine();
+                if (playAgain == "yes"){
+                    keepPlaying = true;
+                } else keepPlaying = false;
+        } while (keepPlaying == true);
     }
 }
