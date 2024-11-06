c021612f55bf244b4e50b36a7c1398fd60af09d2
Jamian221 <ols23045@byui.edu>
2024-11-06 09:36:27 -0700

    finished creating a class

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,44 +2,27 @@ using System;
 
 class Program
 {
-    static int AddNumbers(int n1, int n2){
-        int total = n1 + n2;
-        return total;
+    class Circle
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
     }
     static void Main(string[] args)
     {
-        int total = AddNumbers(20, 30);
-        Console.WriteLine(total);
+        Console.WriteLine("hajimemashite, Ien desu");
+        int x = 10;
 
+        Circle myCircle = new Circle(x);
 
-        // // declare variables
-
-
-
-
-
-
-
-
-        
-        // int age;
-        // do
-        // {
-        //     Console.Write("Please enter age (0-125): ");
-        //     age = int.Parse(Console.ReadLine());
-        //     Console.WriteLine($"Your age is: {age}.");
-
-        // }while( age < 0 || age > 125);
-
-        // List <string> myColors = new List<string>();
-
-        // myColors.Add("Red");
-        // myColors.Add("Green");
-        // myColors.Add("Blue");
-
-        // foreach(string color in myColors)
-        // {
-        //     Console.WriteLine(color);
-        // }
+        Console.WriteLine(myCircle.GetArea());
     }
 }
