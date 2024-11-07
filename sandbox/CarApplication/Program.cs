87188bc7137891d473827e5a55ce6c91d5f2f7df
Jamian221 <ols23045@byui.edu>
2024-11-07 09:54:58 -0700

    started car project

---
+++ sandbox/CarApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+class Program{
+
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello World");
+        Car myHonda = new Car();
+
+        myHonda.Display();
+    }
+}
