e41783c57a790f2c479f58bb6783b55c656491db
Jamian221 <ols23045@byui.edu>
2024-11-09 17:56:05 -0700

    added in 'AddNumbers' function

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,14 +1,30 @@
 using System;
+using System.Globalization;
 
 class Program
 {
+    static int AddNumbers(List<int> numbers)
+    {
+        int addedNumbers = 0;
+        foreach (int number in numbers){
+            addedNumbers += number;
+        }
+        return addedNumbers;
+    }
     static void Main(string[] args)
     {
         // declare variables
         int biggestNumber;
         int average;
         int newNumber;
+        int total;
         List<int> numbers = new List<int>();
-        
+
+        do
+        {
+            Console.Write("What is the next number in the list? ");
+            newNumber = int.Parse(Console.ReadLine());
+            numbers.Add(newNumber);
+        }while (newNumber != 0);
     }
 }
