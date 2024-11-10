2c819e68f36f64f505ad359d049ceb4ad7215a95
Jamian221 <ols23045@byui.edu>
2024-11-09 23:10:59 -0700

    Completed 2 functions on prep 5 assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,30 @@
 using System;
 
+/* 
+DisplayWelcome - Displays the message, "Welcome to the Program!"
+PromptUserName - Asks for and returns the user's name (as a string)
+PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
+SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
+DisplayResult - Accepts the user's name and the squared number and displays them.
+*/ 
+
+
+
 class Program
 {
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName(){
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
     }
 }
