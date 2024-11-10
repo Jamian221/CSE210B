3220972a9e80a7cdd5c492112b7b7b31a41363c6
Jamian221 <ols23045@byui.edu>
2024-11-09 17:48:34 -0700

    declared variables in prep4 assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        // declare variables
+        int biggestNumber;
+        int average;
+        int newNumber;
+        List<int> numbers = new List<int>();
         
     }
 }
