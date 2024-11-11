14faea0e5bf26896e6b2e032ef76a14b13283421
Jamian221 <ols23045@byui.edu>
2024-11-11 09:54:22 -0700

    started resume assignment Created new files and started the job class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,11 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Input your age: ");
-        string userInput = Console.ReadLine();
-        int age = int.Parse(userInput);
-
-        Console.WriteLine($"Your age is {age}!");
-        // Console.WriteLine("Hello Learning02 World!");
+        
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,15 @@
+class Job
+{
+    private string jobStart;
+    private string jobEnd;
+    private string companyName;
+    private string jobTitle;
+
+    public Job(string jobStart, string jobEnd, string companyName, string jobTitle)
+    {
+        this.jobStart = jobStart;
+        this.jobEnd = jobEnd;
+        this.companyName = companyName;
+        this.jobTitle = jobTitle;
+    }
+}
