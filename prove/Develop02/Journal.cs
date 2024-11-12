0779a84bc45e21399fa7ada23b61f658183ebb7f
Jamian221 <ols23045@byui.edu>
2024-11-12 09:41:01 -0700

    started journal program. Created Journal class and added methods

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,19 @@
+class Journal
+{
+    public void DisplayAllEntried()
+    {
+
+    }
+    public void WriteEntriesToFile()
+    {
+
+    }
+    public void CreateEntry()
+    {
+
+    }
+    public void ReadEntriesFromFile()
+    {
+        
+    }
+}
