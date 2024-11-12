87f885dd45a63afbab37501bc063f2df6867aab1
Jamian221 <ols23045@byui.edu>
2024-11-12 09:31:48 -0700

    finished program.cs and finished project.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -3,7 +3,14 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        
+    {   
+        Resume myResume = new Resume("Ian Olson");
+        Job job1 = new Job("2009", "2015", "Amazon", "Evil Scientist");
+        myResume.AddJob(job1);
+
+        Job job2 = new Job("2015", "2020", "Evil Inc.", "Eviler Scientist");
+        myResume.AddJob(job2);
+        myResume.Display();
+
     }
 }
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,16 +1,16 @@
 class Resume 
 {
-    private List<Job> jobs;
+    public List<Job> jobs = new List<Job>();
     private string name;
+    public Resume(string name)
+    {
+        this.name = name;
+    }
     public void AddJob(Job job)
     {
         jobs.Add(job);
     }
-    public void SetName(string inputName)
-    {
-        name = inputName;
-    }
-    public void DisplayResume()
+    public void Display()
     {
         Console.WriteLine($"{name}");
         foreach(Job job in jobs)
