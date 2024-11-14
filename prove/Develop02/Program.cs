cc0912828c072f62935deafe7db0887544cb07a3
Jamian221 <ols23045@byui.edu>
2024-11-14 10:33:44 -0700

    added loop to program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,21 +7,23 @@ class Program
         int menuChoice;
         Journal journal = new Journal();
         Menu menu = new Menu();
-        menu.DisplayMenu();
-        menuChoice = menu.GetInput();
+        while (true){
+            menu.DisplayMenu();
+            menuChoice = menu.GetInput();
 
-        if (menuChoice == 1){
-            JournalEntry Entry = new JournalEntry();
-            journal.AddEntry(Entry);
+            if (menuChoice == 1){
+                JournalEntry Entry = new JournalEntry();
+                journal.AddEntry(Entry);
 
-        }else if (menuChoice == 2){
+            }else if (menuChoice == 2){
 
-        }else if (menuChoice == 3){
+            }else if (menuChoice == 3){
 
-        }else if (menuChoice == 4){
-
-        }else if (menuChoice == 5){
-            menu.Quit();
-        }else Console.WriteLine("Not a valid input, please try again");
+            }else if (menuChoice == 4){
+                journal.DisplayAllEntries();
+            }else if (menuChoice == 5){
+                menu.Quit();
+            }else Console.WriteLine("Not a valid input, please try again");
+        }
     }
 }
