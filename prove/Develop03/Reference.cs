738a2f42be68bf398c75bdd542ef84af92e1b06a
Jamian221 <ols23045@byui.edu>
2024-11-19 15:54:21 -0700

    added commets to develop03

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,8 +1,14 @@
 class Reference{
+    // attributes
     private string _reference;
+
+    // methods
+
+    // constructor that takes a reference string and sets it to an attribute
     public Reference(string reference){
         _reference = reference;
     }
+    // returns reference string
     public string DisplayReference(){
         return _reference;
     }
--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,15 +1,22 @@
 class Word{
+    // attributes
+    // string for if the word isn't hidden
     private string _word;
-    private bool _isHidden;
+    // string for if word is hidden
     private string _underscoreWord;
+    // if the word is hidden
+    private bool _isHidden;
+    // constructor - takes a word as a string, says  word isn't hidden, creates hidden word string (same amount of underscores as letters)
     public Word(string word){
         _word = word;
         _isHidden = false;
         _underscoreWord = "";
+        // for each letter in the word, add an underscore to the hidden word
         for (int i = 0; i < _word.Length; i++){
             _underscoreWord = $"{_underscoreWord}_";
         }
     }
+    // returns the hidden word if hidden and word if it's not
     public string GetWord(){
         if (_isHidden){
             return _underscoreWord;
@@ -18,9 +25,11 @@ class Word{
             return _word;
         }
     }
+    // hides the word
     public void HideWord(){
         _isHidden = true;
     }
+    // returns the bool for if it is hidden
     public bool CheckIfHidden(){
         return _isHidden;
     }
