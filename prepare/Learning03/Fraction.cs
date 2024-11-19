cc66417420099f7fabaf0f21a743acfc6929de0a
Jamian221 <ols23045@byui.edu>
2024-11-18 23:36:10 -0700

    finished step 4 (tested no, one, and two parameter constructors) and tested the GetFractionString() function

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -7,16 +7,24 @@ class Fraction{
         _numerator = numerator;
         _denominator = denominator;
     }
-    public Fraction(){}
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+    public Fraction(int numerator){
+        _numerator = numerator;
+        _denominator = 1;
+    }
 
-    public int GetNumerator()
+    private int GetNumerator()
     {
         return _numerator;
     }
     public void SetNumerator(int numerator){
         _numerator = numerator;
     }
-    public int GetDenominator(){
+    private int GetDenominator(){
         return _denominator;
     }
     public void SetDenominator(int denominator){
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction noArgument = new Fraction();
+        Console.WriteLine(noArgument.GetFractionString());
+
+        Fraction oneArgument = new Fraction(5);
+        Console.WriteLine(oneArgument.GetFractionString());
+
+        Fraction twoArguments = new Fraction(5, 10);
+        Console.WriteLine(twoArguments.GetFractionString());
+
     }
 }
