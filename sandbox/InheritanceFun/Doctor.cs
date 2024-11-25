4a6670ec6600dac6b321b6fb46cac65723ebb91d
Jamian221 <ols23045@byui.edu>
2024-11-25 09:42:42 -0700

    started Doctor.cs file

---
+++ sandbox/InheritanceFun/Doctor.cs
@@ -0,0 +1,7 @@
+class Doctor : Person{
+    private string[] _tools;
+    public Doctor(string firstName, string lastName, int age, string[] tools) :base(firstName, lastName, age)
+    {
+        _tools = tools;
+    }
+}
