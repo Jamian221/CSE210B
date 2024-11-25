d4fd72db567b5dda0ca719ba1372fcee09346bc9
Jamian221 <ols23045@byui.edu>
2024-11-25 12:35:40 -0700

    finished learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        MathAssignment assignment1 = new("Ian Olson", "Geometry", "1.7", "1-11 odd");
+        WritingAssignment assignment2 = new("Ian Olson", "English", "Why are people weird?");
+        Console.WriteLine($"{assignment1.GetHomeworkList()}");
+        Console.WriteLine($"{assignment2.GetWritingInformation()}");
+
     }
 }
