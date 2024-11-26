db49a9c3ceb3329a74fe872a6b6fc994d768dc42
Jamian221 <ols23045@byui.edu>
2024-11-26 09:43:14 -0700

    started develop04 project. Created menu.cs and adapted it for this program

---
+++ prove/Develop04/Menu.cs
@@ -0,0 +1,27 @@
+using System.Net.Quic;
+
+class Menu
+{
+    private List<string> _menuOptions = new List<string>{"1. Start Breathing Activity", "2. Start Reflecting Activity", "3. Start Listing Activity", "4. Quit"};
+    
+    private int _menuChoice;
+    public void DisplayMenu(){
+        Console.WriteLine("Menu Options:");
+        foreach (string option in _menuOptions){
+            Console.WriteLine(option);
+        }
+        Console.WriteLine();
+    }
+    public int GetInput(){
+        Console.Write("Select a choice from the menu: ");
+        _menuChoice = int.Parse(Console.ReadLine());
+        return _menuChoice;
+    }  
+    public void Quit(){
+        Environment.Exit(0);
+    }
+    public void DisplayEntries(){
+
+    }
+    
+}
