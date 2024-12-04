66c16879509be65d9a213111f5cd96974310d7c8
Jamian221 <ols23045@byui.edu>
2024-12-04 11:26:20 -0700

    Created constructors for each class

--- prepare/Learning05/Circle.cs
+++ prepare/Learning05/Circle.cs
@@ -2,6 +2,9 @@ using System.Formats.Asn1;
 
 class Circle :Shape{
     private double _radius;
+    public Circle(string color, double radius) :base(color){
+        _radius = radius;
+    }
     public override double GetArea()
     {
         double area = _radius * _radius * Math.PI;
--- prepare/Learning05/Shape.cs
+++ prepare/Learning05/Shape.cs
@@ -1,5 +1,8 @@
 class Shape{
     private string _color;
+    public Shape(string color){
+        _color = color;
+    }
     public virtual double GetArea(){
         double area = 0;
         return area;
