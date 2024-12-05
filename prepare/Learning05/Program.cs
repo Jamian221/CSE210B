1bd9813baf92dd0fc41bd7777c58a87dbf4ae1f3
Jamian221 <ols23045@byui.edu>
2024-12-04 19:27:12 -0700

    got git working on my computer i think

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -15,7 +15,5 @@ class Program
         foreach (Shape shape in shapes){
             Console.WriteLine($"Shape: {shape.GetShape()}, Color: {shape.GetColor()}, Area: {shape.GetArea()}");
         }
-
-        
     }
 }
