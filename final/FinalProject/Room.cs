dbb99b7946bfe3ad8be7f22b018fdb8de7ade507
Jamian221 <ols23045@byui.edu>
2024-12-11 10:42:08 -0700

    added all functions to all classes. Now I actually need to make the thing work.

--- final/FinalProject/Room.cs
+++ final/FinalProject/Room.cs
@@ -1,3 +1,47 @@
+using System.Runtime.CompilerServices;
+
 class Room {
-    
+    private Character _player;
+    private List<Item> _rewards;
+    private List<Enemy> _enemies;
+    private Creature _attacker;
+    private Creature _defender;
+    private float _chanceOfHitting;
+    private int _attackDamage;
+    private int _attackerSpeed;
+    private int _defenderSpeed;
+    public Room(List<Item> reward, List<Enemy> enemies){
+        _rewards = reward;
+        _enemies = enemies;
+    }
+    public void SetAttackerStats(){
+        _attackerSpeed = _attacker.ReturnSpeed();
+        _attackDamage = _attacker.BasicAttack();
+    }
+    public void SetDefenderStats(){
+        _defenderSpeed = _defender.ReturnSpeed();
+    }
+    public bool CheckIfDead(Creature creature){
+        return creature.ReturnDead();
+    }
+    public void DealDamage(Creature creature){
+        creature.TakeDamage(_attackDamage);
+
+    }
+    public void ClaculateChanceOfHitting(){
+        _chanceOfHitting = _attackerSpeed / _defenderSpeed;
+    }
+    public void SetAttacker(Creature creature){
+        _attacker = creature;
+    }
+    public void SetDefender(Creature creature){
+        _defender = creature;
+    }
+    public void EndFight(){
+
+    }
+    public void EndRoom(){
+        
+    }
+
 }
